18cc5a9ecd27bc340acf0847104fba56be907e99
Bessmack <jenwied4lif@gmail.com>
2025-06-09 16:18:28 +0300

    complete scope

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+customerName;
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'Kim';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Moh';
+    return leastFavoriteCustomer;
+}
